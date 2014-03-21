4bbbac477b5a3889d86caff786e54ba1efc588c5
Sean Hoyt <seanhoyt963@gmail.com>
2014-03-20 23:27:37 -0500

    Liquify just a tiny bit

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
-LOCAL_PACKAGE_NAME := SlimFileManager
+LOCAL_PACKAGE_NAME := LiquidFileManager
 LOCAL_CERTIFICATE := platform
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
